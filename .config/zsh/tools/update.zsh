0c2ae6dc150d00c7e8562a86d6e29123bec99a59
glaringgibbon <35023537+glaringgibbon@users.noreply.github.com>
2025-05-23 01:42:42 +0100

    Update update.zsh

---
+++ .config/zsh/tools/update.zsh
@@ -0,0 +1,4 @@
+# Would this be a better location for the check language function?
+# It is referrenced in the .zshrc adjacent to zprof and there is a file in tools folder called profile.zsh
+# I am assuming that there may be a link between the profile.zsh file and zprof.
+# Can you set options for profiling here or something?
