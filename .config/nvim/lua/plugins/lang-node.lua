93ca75c9384ed553d8022db649981a98d6a3f78c
glaringgibbon <35023537+glaringgibbon@users.noreply.github.com>
2025-05-23 22:02:45 +0100

    Update lang-node.lua

--- .config/nvim/lua/plugins/lang-node.lua
+++ .config/nvim/lua/plugins/lang-node.lua
@@ -1,12 +1,24 @@
 -- $XDG_CONFIG_HOME/nvim/lua/plugins/lang-node.lua
 
+local vim = vim
+local state = require("config.state")
+local notify = vim.notify
+
 return {
   {
     "neovim/nvim-lspconfig",
+    enabled = function()
+      return state.is_language_ready("node")
+    end,
     opts = {
       servers = {
         tsserver = {
-          enabled = require("config.state").is_language_ready("node"),
+          before_init = function(_, config)
+            if not state.validate_provider("node") then
+              notify("Node.js provider not properly configured", vim.log.levels.WARN)
+              -- Don't return false here as Node provider is optional
+            end
+          end,
           settings = {
             typescript = {
               inlayHints = {
@@ -32,7 +44,38 @@ return {
             },
           },
         },
+        eslint = {
+          enabled = function()
+            return state.is_language_ready("node")
+          end,
+        },
       },
     },
+  },
+  {
+    "williamboman/mason.nvim",
+    enabled = function()
+      return state.is_language_ready("node")
+    end,
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, {
+        "typescript-language-server",
+        "eslint-lsp",
+        "prettier",
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if state.is_language_ready("node") then
+        vim.list_extend(opts.ensure_installed or {}, {
+          "javascript",
+          "typescript",
+          "tsx",
+        })
+      end
+    end,
   },
 }
